46fb19e8c05eb39a874b7777d4880e28f74ac910
codedjinn <codedjinn@gmail.com>
2022-05-13 02:23:57 -0700

    assets

---
+++ src/models/level_definition.rs
@@ -0,0 +1,6 @@
+
+pub struct LevelDefinition {
+    // Treat as system name, used to identify levels when progressing or need to teleport
+    name: String,
+    
+}
---
+++ src/shared.rs
@@ -0,0 +1,3 @@
+
+pub const LAYER_0:f32 = 1.0;
+pub const LAYER_1:f32 = 0.0;
